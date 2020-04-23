e6f237c17e8d6f8e4f4ebb6051f10531c9816feb
Mihail Alekseev <mihailalekseev02@gmail.com>
2020-04-23 23:24:47 +0300

    Initializing new project for archiving

---
+++ Lab4_Archiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_Archiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
